767780dbaa357e43b3b73dbe83acab0b46c7e7fc
KawshikBss <kawshikbss@gmail.com>
2022-12-09 00:26:18 +0600

    fixed sidebar current route

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Location, useLocation } from 'react-router-dom'
 
 function Sidebar() {
+    const location = useLocation();
+    const isCurrentRoute = (pathName) => {
+        return pathName === location.pathname? 'current-menu-item': '';
+    };
   return (
     <>
        <div className="navigation" id="navigation">
                     <div className="nav_inner">
                         <ul className="nav_list">
-                            <li className="current-menu-item">
+                            <li className={isCurrentRoute('/')}>
                                 <Link to={'/'}>
                                     <i>
                                         <img src="images/nav1.png" alt="" />
@@ -16,7 +20,7 @@ function Sidebar() {
                                     <span>Dashboard</span>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={isCurrentRoute('/my-videos')}>
                                 <Link to={'/my-videos'}>
                                     <i>
                                         <img src="images/nav2.png" alt="" />
@@ -25,7 +29,7 @@ function Sidebar() {
                                     <span>My Videos</span>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={isCurrentRoute('/my-earnings')}>
                                 <Link to={'/my-earnings'}>
                                     <i>
                                         <img src="images/nav3.png" alt="" />
@@ -34,7 +38,7 @@ function Sidebar() {
                                     <span>My Earnings</span>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={isCurrentRoute('/my-messages')}>
                                 <Link to={'/my-messages'}>
                                     <i>
                                         <img src="images/nav4.png" alt="" />
@@ -43,7 +47,7 @@ function Sidebar() {
                                     <span>My Messages</span>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={isCurrentRoute('/notification')}>
                                 <Link to={'/notification'}>
                                     <i>
                                         <img src="images/nav5.png" alt="" />
@@ -52,7 +56,7 @@ function Sidebar() {
                                     <span>Notifications</span>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={isCurrentRoute('/setting')}>
                                 <Link to={'/setting'}>
                                     <i>
                                         <img src="images/nav6.png" alt="" />
