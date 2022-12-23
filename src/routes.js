2c677d5b6649774577090f4a4079be5f3ad288d3
KawshikBss <kawshikbss@gmail.com>
2022-12-23 18:48:52 +0600

    stored route paths and components

---
+++ src/routes.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const routes = [
+    {
+        path: '/',
+        component: React.lazy(() => import('./routes/Home')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/login',
+        component: React.lazy(() => import('./routes/Login')),
+        layout: 'none',
+        protected: false,
+    },
+    {
+        path: '/forgot-password',
+        component: React.lazy(() => import('./routes/ForgotPassword')),
+        layout: 'none',
+        protected: false,
+    },
+    {
+        path: '/my-videos',
+        component: React.lazy(() => import('./Pages/Videos/Videos')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/my-earnings',
+        component: React.lazy(() => import('./routes/MyEearning')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/my-messages',
+        component: React.lazy(() => import('./routes/MyMessages')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/notification',
+        component: React.lazy(() => import('./routes/Notifications')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/setting',
+        component: React.lazy(() => import('./routes/Setting')),
+        layout: 'default',
+        protected: true,
+    },
+    {
+        path: '/chat-details/:id',
+        component: React.lazy(() => import('./routes/Chat')),
+        layout: 'detail',
+        protected: true,
+    }
+]
+
+export default routes;
