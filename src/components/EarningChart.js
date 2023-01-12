080b21e3adbe85cdef49d65e228b850db4b2a135
KawshikBss <kawshikbss@gmail.com>
2023-01-12 19:01:26 +0600

    fetched data directly from api

--- src/components/EarningChart.js
+++ src/components/EarningChart.js
@@ -1,12 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactApexChart from 'react-apexcharts'
+import useFetch from '../hooks/useFetch';
+import { formatMonth } from '../lib/queryClient';
 
-function EarningChart({ chartData }) {
+function EarningChart({byYear = false}) {
+    
+  const currYear = new Date().getFullYear();
+  const currMonth = new Date().getMonth();
+    const {
+      data: earningByMonths,
+      refetch: refetchEarningByMonths,
+      isLoading: earningByMonthsIsLoading,
+      isSuccess: earningByMonthsSuccess,
+  } = useFetch(["earning-report-by-month"], `/graph-earning/2022/2`);
+  const {
+      data: earningByYears,
+      refetch: refetchEarningByYears,
+      isLoading: earningByYearsIsLoading,
+      isSuccess: earningByYearsSuccess,
+  } = useFetch(["earning-report-by-year"], `/graph-earning-year/2022`);
+  console.log(earningByMonths);
+  console.log(earningByYears);
+  const formatGraphData = (data, byYear = false) => {
+      if (!data) return {
+        revenues: [1, 1, 1, 1],
+        dates: [1, 1, 1, 1],
+      };
+      let formattedData;
+      if(byYear){
+        formattedData = {
+          revenues: Array.from(data, (item) => item.revenue),
+          dates: Array.from(
+              data,
+              (item) => `${formatMonth(currMonth)} ${item.date}th`
+          ),
+      };} else {
+        formattedData = {
+          revenues: Array.from(data, (item) => item.revenue),
+          dates: Array.from(
+              data,
+              (item) => `${currYear} ${formatMonth(parseInt(item.date) - 1)}`
+          ),
+      };
+      }
+      return formattedData;
+  };
 
     const [state,setState]=useState({
         series: [{
-            name: 'series1',
-            data: chartData? chartData: [31, 40, 28, 51, 42, 109, 100]
+            name: 'Revenue',
+            data: byYear? formatGraphData(earningByMonths).revenues: formatGraphData(earningByYears, true).revenues,
           }, ],
           options: {
             colors: ['#72efc5'],
@@ -23,7 +66,7 @@ function EarningChart({ chartData }) {
             xaxis: {
               type: 'date',
               tickAmount: 11,
-              categories: ["Sep 1st", "Sep 4th", "Sep 7th", "Sep 10th", "Sep 13th", "Sep 16th", "Sep 19th", "Sep 21th", "Sep 24th", "Sep 27th", "Sep 30th"],
+              categories: byYear? formatGraphData(earningByMonths).dates: formatGraphData(earningByYears, true).dates,
               labels: {
                 show: true,
                 rotate: 0,
@@ -54,6 +97,19 @@ function EarningChart({ chartData }) {
             },
           },
     })
+    useEffect(() => {
+      setState({...state, series: [{
+        name: 'Revenue',
+        data: byYear? formatGraphData(earningByMonths).revenues: formatGraphData(earningByYears, true).revenues,
+      }, ],
+      options: {
+        ...state.options,
+        xaxis: {
+          ...state.options.xaxis,
+          categories: byYear? formatGraphData(earningByMonths).dates: formatGraphData(earningByYears, true).dates,}
+      }
+    })
+    }, [earningByMonthsIsLoading, earningByYearsIsLoading, byYear])
   return (
     <div>
       <ReactApexChart options={state.options} series={state.series} type="area" height={224} />
