90d76d1510ffd3faaf5d8094b41a427aa16a06b1
KawshikBss <kawshikbss@gmail.com>
2023-01-13 02:19:55 +0600

    created new helper functions for earning summary

--- src/lib/queryClient.js
+++ src/lib/queryClient.js
@@ -3,139 +3,246 @@ import { QueryCache, QueryClient } from "react-query";
 import { format, isToday, isYesterday } from "date-fns";
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 10,
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 10,
+        },
     },
-  },
-  queryCache: new QueryCache({
-    onError: (error, query) => {
-      if (query.state.data !== undefined) {
-        toast.error(`Something went wrong: ${error.message}`);
-      }
-    },
-  }),
+    queryCache: new QueryCache({
+        onError: (error, query) => {
+            if (query.state.data !== undefined) {
+                toast.error(`Something went wrong: ${error.message}`);
+            }
+        },
+    }),
 });
 
 export const notify = (message = "Successfully Updated!", error = false) => {
-  if (!error) {
-    toast.success(message);
-  } else {
-    toast.error(message);
-  }
+    if (!error) {
+        toast.success(message);
+    } else {
+        toast.error(message);
+    }
 };
 
 const getImageURL = (file) => {
-  return "https://vsclips.s3.us-east-2.amazonaws.com/" + file;
+    return "https://vsclips.s3.us-east-2.amazonaws.com/" + file;
 };
 
 export default getImageURL;
 
 const currentYear = new Date().getFullYear();
 export const getYearList = (date) => {
-  const list = [];
-  for (let year = date; year <= currentYear; year++) {
-    list.push({ value: year, label: year });
-  }
-  return list;
+    const list = [];
+    for (let year = date; year <= currentYear; year++) {
+        list.push({ value: year, label: year });
+    }
+    return list;
 };
 
 export const getDateTime = (d) => {
-  const today = d ? new Date(d) : new Date();
-  const date = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-  }).format(today);
+    const today = d ? new Date(d) : new Date();
+    const date = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+    }).format(today);
 
-  const time = new Intl.DateTimeFormat("en-US", {
-    timeStyle: "short",
-  }).format(today);
-  return { date, time: time.toLowerCase(), isSuccess: !!d };
+    const time = new Intl.DateTimeFormat("en-US", {
+        timeStyle: "short",
+    }).format(today);
+    return { date, time: time.toLowerCase(), isSuccess: !!d };
 };
 export const plainDateTime = (d) => {
-  if (d) {
-    return (
-      <>
-        {getDateTime(d).date}
-        <br />
-        {getDateTime(d).time} UTC
-      </>
-    );
-  }
-  return "";
+    if (d) {
+        return (
+            <>
+                {getDateTime(d).date}
+                <br />
+                {getDateTime(d).time} UTC
+            </>
+        );
+    }
+    return "";
 };
 
-export const styledDateTime = (d, joinStr = '.') => {
-  const dateTime = d.split("T");
-  const date = dateTime[0].split("-").reverse().join(joinStr);
-  const tmpTime = dateTime[1].split(".")[0].split(":");
-  const time = `${tmpTime[0] >= 12 ? tmpTime[0] - 12 : tmpTime[0]}.${
-    tmpTime[1]
-  }${tmpTime[0] >= 12 ? "pm" : "am"}`;
+export const styledDateTime = (d, joinStr = ".") => {
+    const dateTime = d.split("T");
+    const date = dateTime[0].split("-").reverse().join(joinStr);
+    const tmpTime = dateTime[1].split(".")[0].split(":");
+    const time = `${tmpTime[0] >= 12 ? tmpTime[0] - 12 : tmpTime[0]}.${
+        tmpTime[1]
+    }${tmpTime[0] >= 12 ? "pm" : "am"}`;
 
-  return { date: date, time: time };
+    return { date: date, time: time };
 };
 
 export const todayDateTime = () => {
-  const date = new Date();
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  return {
-    day: dayNames[date.getDay()],
-    date: date.getDate(),
-    month: monthNames[date.getMonth()],
-  };
+    const date = new Date();
+    const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    return {
+        day: dayNames[date.getDay()],
+        date: date.getDate(),
+        month: monthNames[date.getMonth()],
+    };
 };
 
 export const plainTime = (d) => {
-  if (d) {
-    return <>{getDateTime(d).time}</>;
-  }
-  return "";
+    if (d) {
+        return <>{getDateTime(d).time}</>;
+    }
+    return "";
 };
 export const copyText = (text) => {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => notify("Copied"))
-    .catch(() => notify("copy fail", true));
+    navigator.clipboard
+        .writeText(text)
+        .then(() => notify("Copied"))
+        .catch(() => notify("copy fail", true));
 };
 
 export const getTitle = (date) => {
-  if (isToday(new Date(date))) {
-    return "Today";
-  } else if (isYesterday(new Date(date))) {
-    return "Yesterday";
-  } else {
-    return format(new Date(date), "dd.MM.yyyy");
-  }
+    if (isToday(new Date(date))) {
+        return "Today";
+    } else if (isYesterday(new Date(date))) {
+        return "Yesterday";
+    } else {
+        return format(new Date(date), "dd.MM.yyyy");
+    }
 };
 
 export const formatMonth = (month) => {
-  const thisMonths = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-  ];
-  if (month > 11) {
-    month = month - 11;
-  }
-  return thisMonths[month];
+    const thisMonths = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    if (month > 11) {
+        month = month - 11;
+    }
+    return thisMonths[month];
+};
+
+export const getEarningSummaryMonth = (data = false) => {
+    let summary = [
+        {
+            date: "January",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "February",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "March",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "April",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "May",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "June",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "July",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "August",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "September",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "October",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "November",
+            total: 0,
+            available: false,
+        },
+        {
+            date: "December",
+            total: 0,
+            available: false,
+        },
+    ];
+    if (!data) {return summary;}
+    data.map((item, index) => {
+      summary.forEach(element => {
+        if (item.date === element.date) {
+          element.total = item.total;
+          element.available = true;
+        }
+      });
+    })
+    return summary;
+};
+
+export const getEarningSummaryYear = (data = false) => {
+  const currYear = new Date().getFullYear()
+  let summary = Array.from(Array(currYear - 2000), (_, i) => {
+    return {
+    date: `${i + 2001}`,
+    total: 0,
+    available: false,
+    }
+  });
+  if (!data) {return summary;}
+  data.map((item, index) => {
+    summary.forEach(element => {
+      if (item.date === element.date) {
+        element.total = item.total;
+        element.available = true;
+      }
+    });
+  })
+  return summary;
 }
