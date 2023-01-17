d741e3d75ba8bf0e2754aa7253e1a9957cf52583
KawshikBss <kawshikbss@gmail.com>
2023-01-17 18:08:05 +0600

    fixed dynamic data

--- src/components/SubmissionChart.js
+++ src/components/SubmissionChart.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactApexChart from 'react-apexcharts'
+import useFetch from '../hooks/useFetch'
+import { getSubmissionChart } from '../lib/queryClient';
 
 function SubmissionChart() {
+  
+  const currYear = new Date().getFullYear();
+    const {
+      data: submissions,
+      refetch: refetchSubmissions,
+      isLoading: submissionsIsLoading,
+      isSuccess: submissionsSuccess,
+  } = useFetch(["graph-submissions"], `/graph-submission/${currYear}`);
+  const [data, setData] = useState(getSubmissionChart(submissions));
+  const formatGraphData = (data) => {
+    if (!data) return {
+      accepted: [1, 1, 1, 1],
+      rejected: [1, 1, 1, 1],
+      pending: [1, 1, 1, 1],
+    };
+    let formattedData;
+      formattedData = {
+        accepted: Array.from(data, (item) => item.accepted),
+        rejected: Array.from(data, (item) => item.rejected),
+        pending: Array.from(data, (item) => item.pending),
+        dates: Array.from(
+            data,
+            (item) => item.date
+        ),
+    };
+    return formattedData;
+};
     const [state,setState]=useState({
         series: [{
             name: 'Approved',
-            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+            data: formatGraphData(data).accepted
           }, {
             name: 'Rejected',
-            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+            data: formatGraphData(data).rejected
           }, {
             name: 'Pending',
-            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+            data: formatGraphData(data).pending
           }],
           options: {
             colors:['#3b92fe', '#90c1e7', 'rgb(209, 227, 241)'],
@@ -38,7 +67,7 @@ function SubmissionChart() {
               colors: ['transparent']
             },
             xaxis: {
-              categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+              categories: formatGraphData(data).dates,
             },
             yaxis: {
               title: {
@@ -63,6 +92,10 @@ function SubmissionChart() {
             }
           },
     })
+    useEffect(() => {
+      setData(getSubmissionChart(submissions));
+      console.log(formatGraphData(data));
+    }, [submissions, submissionsIsLoading, submissionsSuccess])
   return (
     <div>
       <ReactApexChart options={state.options} series={state.series} type="bar" height={350} />
