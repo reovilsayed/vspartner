23788d402515670fb20e0a6dd8709e8d1583790f
KawshikBss <kawshikbss@gmail.com>
2023-01-20 16:01:36 +0600

    changed video player

--- src/components/videos/VideoPlayer.js
+++ src/components/videos/VideoPlayer.js
@@ -1,94 +1,104 @@
-import { useEffect, useRef, useState } from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
-import hlsQualitySelector from 'videojs-hls-quality-selector';
-import qualityLevels from 'videojs-contrib-quality-levels';
-import '@videojs/themes/dist/forest/index.css';
+import { useEffect, useRef, useState } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import hlsQualitySelector from "videojs-hls-quality-selector";
+import qualityLevels from "videojs-contrib-quality-levels";
+// import "@videojs/themes/dist/forest/index.css";
+import "@videojs/themes/dist/fantasy/index.css";
 function VideoPlayer(props) {
-  const { src, poster } = props;
-  const videoRef = useRef();
-  const [player, setPlayer] = useState(undefined);
-  const [callFinishVideoAPI, setCallFinishVideoAPI] = useState(false);
-  const [vidDuration, setVidDuration] = useState(50000);
-  const videoId = 'e2280eeb-4cdb-43e7-a34f-36868326b8cb';
-  useEffect(() => {
-    if (player) {
-      player.src({
-        src: src,
-        type: 'application/x-mpegURL',
-        withCredentials: false,
-      });
-      player.poster();
-      setCallFinishVideoAPI(false);
-      setVidDuration(50000);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [videoId, src, poster]);
-
-  useEffect(() => {
-    if (callFinishVideoAPI) {
-      // finishesVideo()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [callFinishVideoAPI]);
-
-  useEffect(() => {
-    const videoJsOptions = {
-      autoplay: false,
-      preload: 'auto',
-      controls: true,
-      //poster: poster,
-      sources: [
-        {
-          src: src,
-          type: 'application/x-mpegURL',
-          withCredentials: false,
-        },
-      ],
-      html5: {
-        nativeAudioTracks: true,
-        nativeVideoTracks: true,
-        nativeTextTracks: true,
-      },
-    };
-
-    const p = videojs(
-      videoRef.current,
-      videoJsOptions,
-      function onPlayerReady() {
-        // console.log('onPlayerReady');
-      }
-    );
-
-    setPlayer(p);
-
-    return () => {
-      if (player) player.dispose();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (player) {
-      player.hlsQualitySelector({ displayCurrentQuality: true });
-    }
-  }, [player]);
-  return (
-    <video
-      ref={videoRef}
-      onLoadedMetadata={(e, px) => {
-        // console.log(e.target.duration);
-        setVidDuration(e.target.duration);
-      }}
-      onTimeUpdate={(e) => {
-        if (e.target.currentTime >= vidDuration - 10) {
-          setCallFinishVideoAPI(true);
+    const { src, poster } = props;
+    console.log(src);
+    const videoRef = useRef();
+    const [player, setPlayer] = useState(undefined);
+    const [callFinishVideoAPI, setCallFinishVideoAPI] = useState(false);
+    const [vidDuration, setVidDuration] = useState(50000);
+    const videoId = "e2280eeb-4cdb-43e7-a34f-36868326b8cb";
+    useEffect(() => {
+        if (player) {
+            player.src({
+                src: src,
+                type: "application/x-mpegURL",
+                withCredentials: false,
+            });
+            player.poster();
+            setCallFinishVideoAPI(false);
+            setVidDuration(50000);
         }
-      }}
-      className="video-js vjs-theme-forest vjs-big-play-centered"
-     
-    ></video>
-  );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [videoId, src, poster]);
+
+    useEffect(() => {
+        if (callFinishVideoAPI) {
+            // finishesVideo()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [callFinishVideoAPI]);
+
+    useEffect(() => {
+        const videoJsOptions = {
+            autoplay: false,
+            preload: "auto",
+            controls: true,
+            //poster: poster,
+            sources: [
+                {
+                    src: src,
+                    type: "application/x-mpegURL",
+                    withCredentials: false,
+                },
+            ],
+            html5: {
+                nativeAudioTracks: true,
+                nativeVideoTracks: true,
+                nativeTextTracks: true,
+            },
+        };
+
+        const p = videojs(
+            videoRef.current,
+            videoJsOptions,
+            function onPlayerReady() {
+                // console.log('onPlayerReady');
+            }
+        );
+
+        setPlayer(p);
+
+        return () => {
+            if (player) player.dispose();
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (player) {
+            player.hlsQualitySelector({ displayCurrentQuality: true });
+        }
+    }, [player]);
+    return (
+        <video
+            id="my-video"
+            className="video video-js vjs-theme-fantasy"
+            controls=""
+            preload="auto"
+            width="100%"
+            height={0}
+            poster={poster}
+            data-setup="{}"
+            ref={videoRef}
+            onLoadedMetadata={(e, px) => {
+                // console.log(e.target.duration);
+                setVidDuration(e.target.duration);
+            }}
+            onTimeUpdate={(e) => {
+                if (e.target.currentTime >= vidDuration - 10) {
+                    setCallFinishVideoAPI(true);
+                }
+            }}
+        >
+            <source src={src} type="video/mp4" />
+        </video>
+    );
 }
 
 export default VideoPlayer;
