a3e6135c5235dbf3db8565a05c0ea345b8d872ce
sayed khan <reovilsayed@gmail.com>
2023-01-21 09:13:27 +0600

    earning graph issue fixed

--- src/components/EarningChart.js
+++ src/components/EarningChart.js
@@ -1,108 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import ReactApexChart from 'react-apexcharts'
+import React, { useEffect, useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
 import useFetch from '../hooks/useFetch';
 import { formatMonth } from '../lib/queryClient';
 
 function EarningChart() {
-    
-  const currYear = new Date().getFullYear();
-  const currMonth = new Date().getMonth();
-    const {
-      data: earning,
-      refetch: refetchEarning,
-      isLoading: earningIsLoading,
-      isSuccess: earningSuccess,
-  } = useFetch(["earning-report"], `/graph-earning`);
-  const formatGraphData = (data) => {
-      if (!data) return {
-        revenues: [0, 0, 0, 0],
-        dates: [0, 0, 0, 0],
-      };
-      let formattedData;
-        formattedData = {
-          revenues: Array.from(data, (item) => item.revenue),
-          dates: Array.from(
-              data,
-              (item) => item.date
-          ),
-      };
-      return formattedData;
-  };
-
-    const [state,setState]=useState({
-        series: [{
-            name: 'Revenue',
-            data: formatGraphData(earning).revenues,
-          }, ],
-          options: {
-            colors: ['#72efc5'],
-            chart: {
-              height: 350,
-              type: 'area'
-            },
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              curve: 'smooth'
-            },
-            xaxis: {
-              type: 'date',
-              tickAmount: 11,
-              categories: formatGraphData(earning).dates,
-              labels: {
-                show: true,
-                rotate: 0,
-                // trim: true,
-                style: {
-                    colors: "#000000",
-                    fontSize: '12px',
-                    fontFamily: 'Cabin, sans-serif',
-                    fontWeight: 600,
-                },
-            }
-            },
-            yaxis: {
-              title: {
-                text: '$',
-                style: {
-                  color: "#525050",
-                  fontSize: '20px',
-                  fontFamily: 'Cabin, sans-serif',
-                  fontWeight: 600,
-              }
-              }
-            },
-            tooltip: {
-              x: {
-                format: 'dd/MM/yy'
-              },
-            },
-          },
-    })
-    useEffect(() => {
-      setState({...state, series: [{
-        name: 'Revenue',
-        data: formatGraphData(earning).revenues,
-      }, ],
-      options: {
-        ...state.options,
-        xaxis: {
-          ...state.options.xaxis,
-          categories: formatGraphData(earning).dates}
-      }
-    })
-    }, [earningIsLoading])
-  return (
-    <div>
-      <ReactApexChart options={state.options} series={state.series} type="area" height={224} />
-    </div>
-  )
+	const [options, setOptions] = useState({
+		chart: {
+			type: 'area',
+			height: 225,
+			redrawOnParentResize: true,
+			redrawOnWindowResize: true,
+			toolbar: {
+				show: false,
+			},
+		},
+		dataLabels: {
+			enabled: false,
+		},
+		stroke: {
+			curve: 'smooth',
+			width: 3,
+			fill: '#00E396',
+		},
+		xaxis: {
+			type: 'date',
+			tickAmount: 11,
+			categories: [],
+			labels: {
+				show: true,
+				rotate: 0,
+				// trim: true,
+				style: {
+					colors: '#000000',
+					fontSize: '12px',
+					fontFamily: 'Cabin, sans-serif',
+					fontWeight: 600,
+				},
+			},
+		},
+		yaxis: {
+			tickAmount: 5,
+			labels: {
+				show: true,
+				style: {
+					colors: '#000000',
+					fontSize: '12px',
+					fontFamily: 'Cabin, sans-serif',
+					fontWeight: 600,
+				},
+			},
+		},
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy',
+			},
+		},
+		yaxis: {
+			title: {
+				text: '$',
+				style: {
+					color: '#525050',
+					fontSize: '20px',
+					fontFamily: 'Cabin, sans-serif',
+					fontWeight: 600,
+				},
+			},
+		},
+		fill: {
+			opacity: 1,
+			colors: ['#00E396', '#fff'],
+			gradient: {
+				shade: 'light',
+				type: 'vertical',
+				shadeIntensity: 0.5,
+				gradientToColors: undefined,
+				inverseColors: true,
+				opacityFrom: 0.7,
+				opacityTo: 0.2,
+			},
+		},
+		responsive: [
+			{
+				breakpoint: 1600,
+				options: {},
+			},
+		],
+	});
+	const [series, setSeries] = useState([
+		{
+			name: 'Revenue',
+			data: [],
+		},
+	]);
+	const { data: earningGraph, refetch, isLoading, isSuccess } = useFetch(['earning-report'], `/graph-earning`);
+	useEffect(() => {
+    if(earningGraph){
+      setSeries([
+        {
+          name: 'Revenue',
+          data: Array.from(earningGraph, item => item.revenue),
+        },
+      ])
+			setOptions({
+        ...options,
+        xaxis:{
+          categories:Array.from(earningGraph, item => item.date)
+        }
+      });
+    }
+	}, [earningGraph]);
+	return (
+		<div>
+			<ReactApexChart options={options} series={series} type='area' height={224} />
+		</div>
+	);
 }
 
-export default EarningChart
-
-      
-      
-      
-      
+export default EarningChart;
