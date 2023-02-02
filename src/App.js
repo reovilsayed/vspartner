8547cc9050f7cad9336cab1b6d41e9360c97f544
KawshikBss <kawshikbss@gmail.com>
2023-02-02 16:39:25 +0600

    addded password reset page

--- src/App.js
+++ src/App.js
@@ -15,75 +15,98 @@ import "./App.css";
 import VideoModal from "./components/videos/VideoModal";
 import ForgotPassword from "./Pages/ForgotPassword";
 import routes from "./routes";
+import RecoverPassword from "./Pages/RecoverPassword";
 
 export const VideoContext = createContext();
 export const VideoLayoutContext = createContext();
 
 function App() {
-  const [modal, setModal] = useState(false);
+    const [modal, setModal] = useState(false);
 
-  const [videoDetails, setVideoDetails] = useState({});
-  const toggle = (video) => {
-    if (video) {
-      setVideoDetails({ ...video });
-    }
-    setModal(!modal);
-  };
-  
-  const [isListView, setIsListView] = useState(true);
-  const handleListView = () => {
-    setIsListView(curr => {
-      return !curr;
-    })
-  }
+    const [videoDetails, setVideoDetails] = useState({});
+    const toggle = (video) => {
+        if (video) {
+            setVideoDetails({ ...video });
+        }
+        setModal(!modal);
+    };
 
-  return (
-    <div className="App">
-      <VideoContext.Provider value={{ videoDetails, setVideoDetails, toggle }}>
-        <VideoLayoutContext.Provider value={{isListView, setIsListView, handleListView}}>
-        <Toaster />
-        <Routes>
-          {routes.map((route, index) => {
-            return (
-              <Route
-                key={index}
-                path={route.path ? route.path : "/"}
-                element={
-                    <RequireAuth loginPath="/login">
-                      {route.layout === "default" ? (
-                        <DefaultLayout>
-                          <Suspense fallback={<div>Loading...</div>}>
-                            <route.component />
-                          </Suspense>
-                        </DefaultLayout>
-                      ): (
-                        <DetailsLayout>
-                          <Suspense fallback={<div>Loading...</div>}>
-                            <route.component />
-                          </Suspense>
-                        </DetailsLayout>
-                      ) 
-                      }
-                    </RequireAuth>
-                }
-              />
-            );
-          })}
-          <Route path="/login" element={<Login />}/>
-          <Route path="/forgot-password" element={<ForgotPassword />}/>
-        </Routes>
-        {modal && (
-          <VideoModal
-            show={modal}
-            toggle={toggle}
-            videoDetails={videoDetails}
-          />
-        )}
-        {/* {modal && <VideoDetailsModal toggle={toggle} videoDetails={videoDetails} />} */}
-        </VideoLayoutContext.Provider>
-      </VideoContext.Provider>
-    </div>
-  );
+    const [isListView, setIsListView] = useState(true);
+    const handleListView = () => {
+        setIsListView((curr) => {
+            return !curr;
+        });
+    };
+
+    return (
+        <div className="App">
+            <VideoContext.Provider
+                value={{ videoDetails, setVideoDetails, toggle }}
+            >
+                <VideoLayoutContext.Provider
+                    value={{ isListView, setIsListView, handleListView }}
+                >
+                    <Toaster />
+                    <Routes>
+                        {routes.map((route, index) => {
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path ? route.path : "/"}
+                                    element={
+                                        <RequireAuth loginPath="/login">
+                                            {route.layout === "default" ? (
+                                                <DefaultLayout>
+                                                    <Suspense
+                                                        fallback={
+                                                            <div>
+                                                                Loading...
+                                                            </div>
+                                                        }
+                                                    >
+                                                        <route.component />
+                                                    </Suspense>
+                                                </DefaultLayout>
+                                            ) : (
+                                                <DetailsLayout>
+                                                    <Suspense
+                                                        fallback={
+                                                            <div>
+                                                                Loading...
+                                                            </div>
+                                                        }
+                                                    >
+                                                        <route.component />
+                                                    </Suspense>
+                                                </DetailsLayout>
+                                            )}
+                                        </RequireAuth>
+                                    }
+                                />
+                            );
+                        })}
+                        <Route path="/login" element={<Login />} />
+                        <Route
+                            path="/forgot-password"
+                            element={<ForgotPassword />}
+                        />
+                        <Route
+                            path="/recover-password"
+                            element={<RecoverPassword />}
+                        />
+                    </Routes>
+                    {modal && (
+                        <VideoModal
+                            show={modal}
+                            toggle={toggle}
+                            videoDetails={videoDetails}
+                        />
+                    )}
+                    {/* {modal && <VideoDetailsModal toggle={toggle} videoDetails={videoDetails} />} */}
+                </VideoLayoutContext.Provider>
+            </VideoContext.Provider>
+        </div>
+    );
 }
 
 export default App;
