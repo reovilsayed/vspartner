9b10443303c579c54db5341fa428f74d4a10f841
KawshikBss <kawshikbss@gmail.com>
2023-02-14 19:30:57 +0600

    added preloader handling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,10 +3,12 @@ import { useAuthHeader, useSignOut } from 'react-auth-kit';
 import { useQuery } from 'react-query';
 import { queryClient } from '../lib/queryClient';
 import requests from '../services/httpService';
+import usePreloader from './usePreloader';
 const useFetch = (key, url, body, options = {}) => {
 	const authHeader = useAuthHeader();
+	const [, setShowLoader] = usePreloader();
 	const signOut = useSignOut();
-	const { prefetch, pagePrefetchKey, ...configOptions } = options;
+	const { prefetch, pagePrefetchKey, noPreLoader, ...configOptions } = options;
 	const { data, isLoading, isError, isSuccess, error, refetch } = useQuery(key, () => requests.post(url, body, { token: authHeader() }), {
 		...configOptions,
 	});
@@ -21,6 +23,10 @@ const useFetch = (key, url, body, options = {}) => {
 			);
 		}
 	}, [pagePrefetchKey, data]);
+	
+	useEffect(() => {
+		!noPreLoader && setShowLoader && setShowLoader(isLoading);
+	}, [isLoading, noPreLoader]);
 	if (isError) {
 		if (error?.response?.status === 401) {
 			signOut();
