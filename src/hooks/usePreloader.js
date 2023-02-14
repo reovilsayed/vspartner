03b283331cb937052c81cc0a2b97cced3f1fa05c
KawshikBss <kawshikbss@gmail.com>
2023-02-14 19:31:18 +0600

    imported for displaying preloader

---
+++ src/hooks/usePreloader.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from "react";
+import Preloader from "../components/layouts/Preloader";
+export const PreloaderContext = createContext([]);
+
+export const PreloaderProvider = ({ children }) => {
+  const [showLoader, setShowLoader] = useState(true);
+  return (
+    <PreloaderContext.Provider value={[showLoader, setShowLoader]}>
+      <div style={{ display: showLoader ? "none" : "block" }}>{children}</div>
+      {showLoader && <Preloader />}
+    </PreloaderContext.Provider>
+  );
+};
+
+const usePreloader = () => {
+  const [showLoader, setShowLoader] = useContext(PreloaderContext);
+  return [showLoader, setShowLoader];
+};
+
+export default usePreloader;
